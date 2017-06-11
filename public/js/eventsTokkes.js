ac7a30f6c05be2fedb31ee0f28e4c2a3f1b3fe21
José Ángel <barrera@estudiodepublicidad.com>
2017-06-12 00:52:33 +0200

    pug files delivered

--- public/js/eventsTokkes.js
+++ public/js/eventsTokkes.js
@@ -1,45 +1,93 @@
-var $testingQuestion = $('select.questionoptionsred').data("questionoptionsred");
-var $testingAnswer = $('div.myanswersred').data("myanswersred");
+$('#phoneBrand_button').on('click', function(e) {
+    e.preventDefault()
+    var queryModelPhone = $('#phoneModel_input').val();
+    var queryPhone = $('#phoneBrand_input').val();
+    var token = '1c7b29cd95da5b687c206483bef8e8e64da31257abe9755f';
+
+    console.log(queryPhone)
+    var urlPhone = 'https://fonoapi.freshpixl.com/v1/getdevice?token=' + token + '&device=' + queryModelPhone + '&brand=' + queryPhone
+
+    $.ajax({
+        url: urlPhone,
+        success: function(data) {
+            var nameBrand = data
+            var haveNFC = ''
+            var noHaveNFC = ''
+
+            nameBrand.forEach(function(phoneName) {
+
+                const deviceName = phoneName.DeviceName.toLowerCase()
+                const deviceBrand = phoneName.Brand.toLowerCase()
+
+                if (phoneName.nfc === 'Yes' || phoneName.nfc === 'NFC') {
+                    haveNFC += '<li>' + phoneName.DeviceName + ' del año ' + phoneName.announced.substr(0, 4) + ' SI es compatible con Tøkkes </li>'
+
+                } else if (phoneName.nfc !== 'NFC' || phoneName.nfc !== 'Yes') {
+
+                    noHaveNFC += '<li>' + phoneName.DeviceName + ' del año ' + phoneName.announced.substr(0, 4) + ', no es compatible con Tøkkes </li>'
+
+                } else {
+                    noHaveNFC += '<li> Su elección no devuelve resultados. Por favor sea más específico en su búsqeda.</li>'
+                }
+
+                $("#list-phone-group").html('<h5 class="text-center"> Estos son los teléfonos compatibles que coinciden con su búsqueda:</h5>' + haveNFC)
+                console.log(deviceBrand)
 
 
-console.log( $testingQuestion)
-console.log($testingAnswer)
+            })
+        }
+    })
+})
 
 
-$(function() {
-	$('#'+ $testingQuestion).change(function() {
-		$('#' + $testingAnswer + '> div').hide();
-		$('#' + $testingAnswer).find('#' + $(this).val()).show();
-		console.log(this)
-	});
-});
 
 
-var $testingQuestion = $('select.questionoptionsred').data("questionoptionsred");
-
-composition = function(){
-	var idSelected = "'" + '#'+ $testingQuestion + "'"
-	return idSelected.toString()
-}
-
-console.log(composition())
-
-this works but it is the same for all tokkes cards
 
 
-quiero alcanzar al select con id del tokke que me interesa
 
-$(function() {
-	$('#questionoptions').change(function() {
-		$('#myanswers > div').hide();
-		$('#myanswers').find('#' + $(this).val()).show();
-		console.log(this)
-	});
-});
 
-	$('#questionoptions').change(function() {
-		$('#myanswers > div').hide();
-		$('#myanswers').find('#' + $(this).val()).show();
-		console.log(this)
-	});
-});
+
+// var $testingQuestion = $('select.questionoptionsred').data("questionoptionsred");
+// var $testingAnswer = $('div.myanswersred').data("myanswersred");
+
+// console.log( $testingQuestion)
+// console.log($testingAnswer)
+
+
+// $(function() {
+// 	$('#'+ $testingQuestion).change(function() {
+// 		$('#' + $testingAnswer + '> div').hide();
+// 		$('#' + $testingAnswer).find('#' + $(this).val()).show();
+// 		console.log(this)
+// 	});
+// });
+
+
+// var $testingQuestion = $('select.questionoptionsred').data("questionoptionsred");
+
+// composition = function(){
+// 	var idSelected = "'" + '#'+ $testingQuestion + "'"
+// 	return idSelected.toString()
+// }
+
+// console.log(composition())
+
+// this works but it is the same for all tokkes cards
+
+
+// quiero alcanzar al select con id del tokke que me interesa
+
+// $(function() {
+// 	$('#questionoptions').change(function() {
+// 		$('#myanswers > div').hide();
+// 		$('#myanswers').find('#' + $(this).val()).show();
+// 		console.log(this)
+// 	});
+// });
+
+// 	$('#questionoptions').change(function() {
+// 		$('#myanswers > div').hide();
+// 		$('#myanswers').find('#' + $(this).val()).show();
+// 		console.log(this)
+// 	});
+// });
--- server/app.js
+++ server/app.js
@@ -15,43 +15,44 @@ mongoose.connect(DB_URI)
 const app = express()
 
 app.set('view engine', 'pug')
-app.set('views', path.join( __dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static( path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
-const queryAreSellingSingle = { "sellingSingleOption" : {exist: true} }
-const queryAreBoxes = { "type.single" :  { $exists: false } }
-const queryAreSingle = { "type.single" : { $exists: true } }
+const queryAreSellingSingle = { "sellingSingleOption": { exist: true } }
+const queryAreBoxes = { "type.single": { $exists: false } }
+const queryAreSingle = { "type.single": { $exists: true } }
 
+// single tokke route
 
-app.get('/tokkes', (req,res) => {
+app.get('/tokkes', (req, res) => {
     Product.find(queryAreSellingSingle)
-    .then( tokkes => {
-    	res.render('buy/tokkes/index', {tokkes})
-    })
+        .then(tokkes => {
+            res.render('buy/tokkes/index', { tokkes })
+        })
 })
 
+// easy packs route
 
-app.get('/boxes', (req,res) => {
-	Product.find( queryAreBoxes )
-	  .populate('type.box.units')
-	  .exec(function(error, boxes) {
-	      res.json( boxes )
-	  })
+app.get('/box/easyPack/:numItems', (req, res) => {
+    const { numItems } = req.params
+    const queryBoxWithNumItems = { $where: `this.type.box.units.length === ${numItems}` }
+    console.log(queryBoxWithNumItems)
+    Product.find(queryBoxWithNumItems)
+        .then(boxTokkes => {
+            res.render('buy/boxes/easyPack/index', { boxTokkes: boxTokkes[0].type.box.units })
+        })
 })
 
-app.get('/box/:numItems', (req,res) => {
+// box love route
 
-	const { numItems } = req.params
-	const queryBoxWithNumItems =  { $where: `this.type.box.units.length === ${numItems}` }
-
-	console.log(queryBoxWithNumItems)
-
-	Product.find( queryBoxWithNumItems )
-	  .populate('type.box.units')
-	  .exec(function(error, boxes) {
-	      res.json( boxes )
-	  })
+app.get('/box/love', (req, res) => {
+    const queryBoxWithNumItems = { $where: `this.type.box.units.length === 12` }
+    console.log(queryBoxWithNumItems)
+    Product.find(queryBoxWithNumItems)
+        .then(boxTokkes => {
+            res.render('buy/boxes/boxLove/index', { boxTokkes: boxTokkes[0].type.box.units })
+        })
 })
 
 app.listen(PORT)
@@ -59,3 +60,47 @@ console.log(`Listening on PORT ${PORT}`)
 console.log(`Listening db: ${DB_URI}`)
 
 
+app.get('/contact', (req, res) => { res.render('forms/contact.pug') })
+app.get('/login', (req, res) => { res.render('forms/login.pug') })
+app.get('/recover', (req, res) => { res.render('forms/recover.pug') })
+
+
+// ***************** //
+
+/*
+
+// it jsons selling single
+
+app.get('/tokkes/json', (req,res) => {
+    Product.find(queryAreSellingSingle)
+  .exec(function(error, tokkes) {
+	      res.json( tokkes )
+    })
+})
+
+// it returns a json with the individual tokkes
+
+app.get('/json', (req,res) => {
+	Product.find( queryAreBoxes )
+	  .populate('type.box.units')
+	  .find( queryAreSingle )
+	  .exec(function(error, boxTokkes) {
+	      res.json( boxTokkes )
+	  })
+})
+
+
+// it returns a json with number of tokkes included in eack box (4, 8, 12)
+
+app.get('/box/json/:numItems', (req,res) => {
+	const { numItems } = req.params
+	const queryBoxWithNumItems =  { $where: `this.type.box.units.length === ${numItems}` }
+	console.log(queryBoxWithNumItems)
+	Product.find( queryBoxWithNumItems )
+	  .populate('type.box.units')
+	  .exec(function(error, boxTokkes) {
+	      res.json( boxTokkes[0].type.box.units )
+	  })
+})
+
+*/
